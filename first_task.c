58cb4d2a9c80ecbb8a99a23e9da580bdc17d49a4
Feri0726 <feherferi2007@gmail.com>
2025-10-01 14:52:31 +0000

    First task complete

--- first_task.c
+++ first_task.c
@@ -9,18 +9,49 @@ Write a program that tabulates the values of a, a3, |a| and sin(a) using 4 decim
 
 */
 #include <stdio.h>
+#include <math.h>
 
-???cube()//<-- Function header, input parameters and output type in this form oType NAME(iType1 iName1,iType2 iName2,...)
+double cube(double a)//<-- Function header, input parameters and output type in this form oType NAME(iType1 iName1,iType2 iName2,...)
 {//here starts what to do
-
-//reurn with the result of the funcion (if any)
-}//that closes the function
+  double al = 1.0;
+  for (int i = 0; i < 3;i++){
+    al = al * a;
+  }
+  return al;
+}
+//reurn with the result of the funcion (if any)//that closes the function
 
 /*Write the other functions here:*/
+double absolute(double a)
+{
+  
+  if (a >= 0){
+    double abs = a;
+  return abs;}
+  else{
+    double abs = -a;
+    return abs;
+  }
 
+}
+double sinu(double a)
+{
+  double val = sin(a);
+  return val;
+}
 
 int main() //main is a function!
 {
+  printf("Give");
+  double a;
+  scanf("%lf", &a);
+  printf("The cube of %f is %f, The absolute value of %f is %f\n", a, cube(a), a, absolute(a));
+  double n = -1;
+  while (n <= 1){
+    printf("%.4f\n", sinu(n));
+    n += 0.1;
+  }
+
 //call the functions in a loop to print the results in the required manner
 
   return 0; //it returns an integer (as promised in the header) which is 0 for now!
