1d43b22dac205ea2a51027965b52f5572958f039
Feri0726 <feherferi2007@gmail.com>
2025-10-01 16:09:55 +0000

    ready twin

--- third_task.c
+++ third_task.c
@@ -1,19 +1,40 @@
 /* Task description:
-A twin prime is a prime number that is either 2 less or 2 more than another prime number - for example, either member of the twin prime pair (41, 43). 
+A twin prime is a prime number that is either 2 less or 2 more than another prime number - for example, either member of the twin prime pair (41, 43).
 In other words, a twin prime is a prime that has a prime gap of two.
 
-The first couple of twin primes are (3, 5), (5, 7) and (11, 13). Write a C program to find the 60th twin prime! 
-Extend the program to check whether the number between the twin primes has any digit equal to 5. 
+The first couple of twin primes are (3, 5), (5, 7) and (11, 13). Write a C program to find the 60th twin prime!
+Extend the program to check whether the number between the twin primes has any digit equal to 5.
 Avoid code duplication, use functions wherever reasonable! Follow the top-down design methodology.
 Top-down: when solving programming problems by breaking them down into smaller and smaller sub-problems until each one is trivial or known
 
 */
 
-
 #include <stdio.h>
+int prime(int x)
+{
+  int wher = 0;
+  for (int i = 2; i < x; i++)
+  {
+    if (x % (i) == 0)
+    {
+      return 0;
+    }}
 
-int main(){
+    return 1;
 
-
+}
+int main()
+{
+  int num=0;
+  int most;
+  int i = 2;
+  while (num < 60)
+  {
+    if (prime(i) == 1&& prime(i+2))
+      num++;
+    i++;
+  }
+  printf("%d", i-1);
+  printf("%d", i+1);
   return 0;
 }
